6a4ef6bb613022f75af822c90ba4b9368975d049
siddharth <siddharth.daftari@gmail.com>
2017-11-04 20:39:24 -0700

    Weather icon added - City detail view

--- app/src/main/java/com/apoorv/android/weatherapp/helper/Constants.java
+++ app/src/main/java/com/apoorv/android/weatherapp/helper/Constants.java
@@ -20,6 +20,7 @@ public class Constants {
     public static final String CURRENT_WEATHER_API_PROP_MAIN_TEMP_MAX = "temp_max";
     public static final String CURRENT_WEATHER_API_PROP_WEATHER = "weather";
     public static final String CURRENT_WEATHER_API_PROP_WEATHER_MAIN = "main";
+    public static final String CURRENT_WEATHER_API_PROP_WEATHER_ICON = "icon";
 
     public static final String FORECAST_WEATHER_API_PROP_CITY = "city";
     public static final String FORECAST_WEATHER_API_PROP_CITY_NAME = "name";
@@ -40,5 +41,24 @@ public class Constants {
 
     public static final String SELECTED_PLACE = "selectedPlace";
     public static final String CITY_LIST_ADAPATER = "cityListAdapter";
+
+    public static final String ICON_01D = "icon_01d";
+    public static final String ICON_01N = "icon_01n";
+    public static final String ICON_02D = "icon_02d";
+    public static final String ICON_02N = "icon_02n";
+    public static final String ICON_03D = "icon_03d";
+    public static final String ICON_03N = "icon_03n";
+    public static final String ICON_04D = "icon_04d";
+    public static final String ICON_04N = "icon_04n";
+    public static final String ICON_09D = "icon_09d";
+    public static final String ICON_09N = "icon_09n";
+    public static final String ICON_10D = "icon_10d";
+    public static final String ICON_10N = "icon_10n";
+    public static final String ICON_11D = "icon_11d";
+    public static final String ICON_11N = "icon_11n";
+    public static final String ICON_13D = "icon_13d";
+    public static final String ICON_13N = "icon_13n";
+    public static final String ICON_50D = "icon_50d";
+    public static final String ICON_50N = "icon_50n";
 
 }
